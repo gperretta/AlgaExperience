6c3277bd6981b6daa16c8393c04874d5c98bd419
gperretta <113616815+gperretta@users.noreply.github.com>
2022-12-07 10:21:16 +0100

    Cleaned up the Xcode Game template.

--- AlgaExperience/AlgaExperience/GameViewController.swift
+++ AlgaExperience/AlgaExperience/GameViewController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController {
 
@@ -15,20 +14,20 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            let scene = GameScene()
+            scene.scaleMode = .aspectFill
+            
+            view.presentScene(scene)
             
             view.ignoresSiblingOrder = true
             
             view.showsFPS = true
             view.showsNodeCount = true
         }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
